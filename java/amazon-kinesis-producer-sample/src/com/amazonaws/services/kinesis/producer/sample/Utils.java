6e93f6a5b7eb1a6b7f01e8b0e9c8422a78993fd1
mnog0 <mnog0@hotmail.com>
2019-08-26 09:52:14 +0000

    KinesisにダミーのSMTPメッセージを送信するように修正
    ・Util.generateDataメソッドにSMTP電文のダミーデータを付与するように修正
    ・pom.xmlにjavamail関連の定義を追加

--- java/amazon-kinesis-producer-sample/src/com/amazonaws/services/kinesis/producer/sample/Utils.java
+++ java/amazon-kinesis-producer-sample/src/com/amazonaws/services/kinesis/producer/sample/Utils.java
@@ -19,22 +19,33 @@ import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.util.Random;
+import java.util.Properties;
+import java.util.Date;
+
+import javax.mail.Session;
+import javax.mail.Address;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+//import javax.mail.PasswordAuthentication;
+//import javax.mail.internet.AddressException;
+//import javax.mail.MessagingException;
+import javax.mail.Message;
 
 public class Utils {
     private static final Random RANDOM = new Random();
-    
+
     /**
      * @return A random unsigned 128-bit int converted to a decimal string.
      */
     public static String randomExplicitHashKey() {
         return new BigInteger(128, RANDOM).toString(10);
     }
-    
+
     /**
      * Generates a blob containing a UTF-8 string. The string begins with the
      * sequence number in decimal notation, followed by a space, followed by
      * padding.
-     * 
+     *
      * @param sequenceNumber
      *            The sequence number to place at the beginning of the record
      *            data.
@@ -44,16 +55,47 @@ public class Utils {
      * @return ByteBuffer containing the blob
      */
     public static ByteBuffer generateData(long sequenceNumber, int totalLen) {
+
+        final String to = "to_xon_test_account@xon.local";
+        final String from = "from_xon_test_account@xon.local";
+        final String charset = "ISO-2022-JP";
+        //final String charset = "UTF-8";
+        final String encoding = "base64";
+
+        String subject = "JavaMail テストメール";
+        String context = "テストメールの本文";
+
         StringBuilder sb = new StringBuilder();
         sb.append(Long.toString(sequenceNumber));
         sb.append(" ");
-        while (sb.length() < totalLen) {
+        sb.append(send(to, from, subject, context));
+        /*while (sb.length() < totalLen) {
             sb.append("a");
-        }
+        }*/
         try {
             return ByteBuffer.wrap(sb.toString().getBytes("UTF-8"));
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String send(String from, String to, String subject, String text) {
+        try {
+            Properties prop = new Properties();
+            prop.put("mail.smtp.host", "localhost");
+            prop.put("mail.smtp.port", "localhost");
+            Session session = Session.getDefaultInstance(prop);
+            MimeMessage mime = new MimeMessage(session);
+            mime.addFrom(InternetAddress.parse(from));
+            Address[] tos = { new InternetAddress(to) };
+            mime.setRecipients(Message.RecipientType.TO, tos);
+            mime.setSubject(subject, "iso-2022-jp");
+            mime.setText(text);
+            mime.setSentDate(new Date());
+            return mime.toString();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
