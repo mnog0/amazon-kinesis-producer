60361bfa0dd3c861d25beb750853247feb180579
mnog0 <mnog0@hotmail.com>
2019-08-28 11:54:45 +0000

    60s 500tps

--- java/amazon-kinesis-producer-sample/src/com/amazonaws/services/kinesis/producer/sample/SampleProducer.java
+++ java/amazon-kinesis-producer-sample/src/com/amazonaws/services/kinesis/producer/sample/SampleProducer.java
@@ -80,7 +80,7 @@ public class SampleProducer {
     /**
      * Put records for this number of seconds before exiting.
      */
-    private static final int SECONDS_TO_RUN = 10;
+    private static final int SECONDS_TO_RUN = 60;
 
     /**
      * Put this number of records per second.
@@ -95,7 +95,7 @@ public class SampleProducer {
      *
      * @see {@link KinesisProducerConfiguration#setRecordTtl(long)}
      */
-    private static final int RECORDS_PER_SECOND = 1000;
+    private static final int RECORDS_PER_SECOND = 500;
 
     /**
      * Change this to your stream name.
