523675419eb8caa4d802c0329c9d37917b73d964
mnog0 <mnog0@hotmail.com>
2019-08-28 09:54:28 +0000

    debug

--- java/amazon-kinesis-producer-sample/src/com/amazonaws/services/kinesis/producer/sample/SampleConsumer.java
+++ java/amazon-kinesis-producer-sample/src/com/amazonaws/services/kinesis/producer/sample/SampleConsumer.java
@@ -124,10 +124,10 @@ public class SampleConsumer implements IRecordProcessorFactory {
 
                     // temp: select keywork from knowledge
                     String keyword = "0000";
-                    if (message.indexOf(keyword) != -1) { log.info(message); }
+                    if (message.indexOf("000") != -1) { log.info(seq); }
+                    if (message.indexOf("0000") != -1) { log.info(message); }
 
-                    //log.info(seq);
-                    log.debug(message);
+                    //log.debug(message);
                     seqNos.add(Long.parseLong(seq));
                 } catch (Exception e) {
                     log.error("Error parsing record", e);
